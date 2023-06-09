8a3849229b35785adeec5e08620a120418c58072
Shist <shist.pupust@mail.ru>
2023-06-09 04:32:57 +0300

    Burger menu button was refactored: now it has exact position and states

--- scripts/toggleBurgerMenu.js
+++ scripts/toggleBurgerMenu.js
@@ -1,22 +1,28 @@
 "use strict";
 
-const burgerMenu = document.querySelector(".burger-menu");
-document
-  .querySelector(".header__burger-menu-btn")
-  .addEventListener("click", () => {
-    burgerMenu.classList.remove("hidden-element");
-    burgerMenu.classList.add("appeared-flex");
-  });
-document
-  .querySelector(".burger-menu__close-btn")
-  .addEventListener("click", () => {
+document.addEventListener("DOMContentLoaded", () => {
+  const burgerMenu = document.querySelector(".burger-menu");
+  const burgerBtn = document.querySelector(".header__burger-menu-btn");
+  const hideMenu = () => {
     burgerMenu.classList.remove("appeared-flex");
     burgerMenu.classList.add("hidden-element");
+    burgerBtn.classList.remove("burger-btn-close");
+    burgerBtn.classList.add("burger-btn-open");
+  };
+  burgerBtn.addEventListener("click", () => {
+    if (window.getComputedStyle(burgerMenu, null).display === "flex") {
+      hideMenu();
+    } else {
+      burgerMenu.classList.remove("hidden-element");
+      burgerMenu.classList.add("appeared-flex");
+      burgerBtn.classList.remove("burger-btn-open");
+      burgerBtn.classList.add("burger-btn-close");
+    }
   });
-const tabletL = 768;
-window.addEventListener("resize", () => {
-  if (this.outerWidth > tabletL) {
-    burgerMenu.classList.remove("appeared-flex");
-    burgerMenu.classList.add("hidden-element");
-  }
+  const tabletL = 768;
+  window.addEventListener("resize", () => {
+    if (this.outerWidth > tabletL) {
+      hideMenu();
+    }
+  });
 });
